8b1db76e80756fd213f332a3302b213bd04271a6
Rafael Estrada <rafaelestrada@comcast.net>
2021-09-11 21:55:25 -0700

    fixed grid

--- js/script.js
+++ js/script.js
@@ -83,3 +83,17 @@ function linkAction(){
 
 navLink.forEach(n => n.addEventListener('click',linkAction))
 console.log('Remove menu profile is working')
+
+
+// ===================Typewriter Effect==============================
+//import Typewriter from 'typewriter-effect/dist/core';
+
+
+
+new Typewriter('#typewriter', {
+  strings: ['Rafael Estrada', 'Web Developer','FullStack','React','SQL','Javascript','MongoDB'],
+  autoStart: true,
+  loop:true,
+  cursor:'|',
+});
+console.log("Typewriter effect is working")
